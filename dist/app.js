9e7bcecd93c775bc2256573c0d2691f837a0c9a6
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-11-14 11:39:30 +0100

    Initializing typescript config settings

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+function addno(n1, n2) {
+    return n1 + n2;
+}
+function printResults(num) {
+    console.log("Result: " + num);
+}
+printResults(addno(5, 7));
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+function addno(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+function printResults(num: number) {
+  console.log("Result: " + num);
+}
+
+printResults(addno(5, 7));
